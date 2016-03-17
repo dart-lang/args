dart-lang/args

commit ff4dd9a8e2255f998fd2f017205d9f908f82b8de

Don't truncate multi-line command descriptions.
Closes #42

R=rnystrom@google.com

Review URL: https://codereview.chromium.org//1603063004 .

lib/command_runner.dart
@@ -371,9 +371,15 @@ String _getCommandUsage(Map<String, Command> commands,
   var buffer =
       new StringBuffer('Available ${isSubcommand ? "sub" : ""}commands:');
   for (var name in names) {
+    var lines = commands[name].description.split("\n");
     buffer.writeln();
-    buffer.write('  ${padRight(name, length)}   '
-        '${commands[name].description.split("\n").first}');
+    buffer.write('  ${padRight(name, length)}   ${lines.first}');
+
+    for (var line in lines.skip(1)) {
+      buffer.writeln();
+      buffer.write(' ' * (length + 5));
+      buffer.write(line);
+    }
   }
 
   return buffer.toString();

pubspec.yaml
@@ -1,5 +1,5 @@
 name: args
-version: 0.13.3-dev
+version: 0.13.3+1
 author: "Dart Team <misc@dartlang.org>"
 homepage: https://github.com/dart-lang/args
 description: >

test/command_runner_test.dart
@@ -54,6 +54,25 @@ Available commands:
 Run "test help <command>" for more information about a command."""));
     });
 
+    test("supports newlines in command descriptions", () {
+      runner.addCommand(new MultilineCommand());
+
+      expect(runner.usage, equals("""
+A test command runner.
+
+Usage: test <command> [arguments]
+
+Global options:
+-h, --help    Print this usage information.
+
+Available commands:
+  help        Display help information for test.
+  multiline   Multi
+              line.
+
+Run "test help <command>" for more information about a command."""));
+    });
+
     test("contains custom options", () {
       runner.argParser.addFlag("foo", help: "Do something.");
 

test/utils.dart
@@ -27,6 +27,18 @@ class FooCommand extends Command {
   }
 }
 
+class MultilineCommand extends Command {
+  var hasRun = false;
+
+  final name = "multiline";
+  final description = "Multi\nline.";
+  final takesArguments = false;
+
+  void run() {
+    hasRun = true;
+  }
+}
+
 class HiddenCommand extends Command {
   var hasRun = false;
 

.gitignore
@@ -3,6 +3,7 @@
 .pub/
 build/
 packages
+.packages
 
 # Or the files created by dart2js.
 *.dart.js

commit 0160fae46e40562acafd15c793d59a38575568a8

Add an explicit distinction between a command's description and summary.
ff4dd9a8e2255f998fd2f017205d9f908f82b8de broke some users who were
relying on only the first line of a command's description being included
in its runner's usage as a summary. That is once again the default
behavior.

To support the use case described in #42, there's a new Command.summary
getter that explicitly controls the summary included in the parent
runner's usage. This defaults to the first line of the description, but
may be overridden to declare a multi-line summary.

Closes #42

R=rnystrom@google.com

Review URL: https://codereview.chromium.org//1812923003 .

lib/command_runner.dart
@@ -190,8 +190,14 @@ abstract class Command {
   /// The name of this command.
   String get name;
 
-  /// A short description of this command.
+  /// A description of this command, included in [usage].
   String get description;
+
+  /// A short description of this command, included in [parent]'s
+  /// [CommandRunner.usage].
+  ///
+  /// This defaults to the first line of [description].
+  String get summary => description.split("\n").first;
 
   /// A single-line template for how to invoke this command (e.g. `"pub get
   /// [package]"`).
@@ -371,7 +377,7 @@ String _getCommandUsage(Map<String, Command> commands,
   var buffer =
       new StringBuffer('Available ${isSubcommand ? "sub" : ""}commands:');
   for (var name in names) {
-    var lines = commands[name].description.split("\n");
+    var lines = commands[name].summary.split("\n");
     buffer.writeln();
     buffer.write('  ${padRight(name, length)}   ${lines.first}');
 

pubspec.yaml
@@ -1,5 +1,5 @@
 name: args
-version: 0.13.3+6
+version: 0.13.4
 author: "Dart Team <misc@dartlang.org>"
 homepage: https://github.com/dart-lang/args
 description: >

test/command_runner_test.dart
@@ -54,8 +54,26 @@ Available commands:
 Run "test help <command>" for more information about a command."""));
     });
 
-    test("supports newlines in command descriptions", () {
+    test("truncates newlines in command descriptions by default", () {
       runner.addCommand(new MultilineCommand());
+
+      expect(runner.usage, equals("""
+A test command runner.
+
+Usage: test <command> [arguments]
+
+Global options:
+-h, --help    Print this usage information.
+
+Available commands:
+  help        Display help information for test.
+  multiline   Multi
+
+Run "test help <command>" for more information about a command."""));
+    });
+
+    test("supports newlines in command summaries", () {
+      runner.addCommand(new MultilineSummaryCommand());
 
       expect(runner.usage, equals("""
 A test command runner.

test/utils.dart
@@ -39,6 +39,10 @@ class MultilineCommand extends Command {
   }
 }
 
+class MultilineSummaryCommand extends MultilineCommand {
+  String get summary => description;
+}
+
 class HiddenCommand extends Command {
   var hasRun = false;
 