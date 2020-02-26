28a5d2f6682b8b14cc9b7dbf55b26d20a48cd802
Devon Carew <devoncarew@google.com>
2020-02-26 12:48:59 -0500

    address an issue when using ArgParser.allowAnything() with CommandRunner (#132)
    address an issue when using allowAnything

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -207,6 +207,24 @@ class AsyncCommand extends Command {
   Future run() => Future.value().then((_) => hasRun = true);
 }
 
+class AllowAnythingCommand extends Command {
+  var hasRun = false;
+
+  @override
+  final name = 'allowAnything';
+
+  @override
+  final description = 'A command using allowAnything.';
+
+  @override
+  final argParser = ArgParser.allowAnything();
+
+  @override
+  void run() {
+    hasRun = true;
+  }
+}
+
 void throwsIllegalArg(function, {String reason}) {
   expect(function, throwsArgumentError, reason: reason);
 }
