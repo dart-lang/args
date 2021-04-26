8d82f3e2b38759279421455682b6641ce4d75043
Kevin Moore <kevmoo@users.noreply.github.com>
2021-04-26 07:10:50 -0700

    Fix newly enforced lint (#193)

--- test/trailing_options_test.dart
+++ test/trailing_options_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:test/test.dart';
 import 'package:args/args.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('allowTrailingOptions defaults to true', () {
