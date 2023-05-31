1be2759345603d8cee594241b2562c9868d1823c
Devon Carew <devoncarew@gmail.com>
2023-05-31 16:25:46 -0700

    address example lints

--- example/command_runner/draw.dart
+++ example/command_runner/draw.dart
@@ -33,7 +33,7 @@ class SquareCommand extends Command<String> {
 
   @override
   FutureOr<String>? run() {
-    final size = int.parse(argResults?['size'] ?? '20');
+    final size = int.parse(argResults?['size'] as String? ?? '20');
     final char = (globalResults?['char'] as String?)?[0] ?? '#';
     return draw(size, size, char, (x, y) => true);
   }
@@ -55,7 +55,7 @@ class CircleCommand extends Command<String> {
 
   @override
   FutureOr<String>? run() {
-    final size = 2 * int.parse(argResults?['radius'] ?? '10');
+    final size = 2 * int.parse(argResults?['radius'] as String? ?? '10');
     final char = (globalResults?['char'] as String?)?[0] ?? '#';
     return draw(size, size, char, (x, y) => x * x + y * y < 1);
   }
@@ -93,7 +93,7 @@ class EquilateralTriangleCommand extends Command<String> {
 
   @override
   FutureOr<String>? run() {
-    final size = int.parse(argResults?['size'] ?? '20');
+    final size = int.parse(argResults?['size'] as String? ?? '20');
     final char = (globalResults?['char'] as String?)?[0] ?? '#';
     return drawTriangle(size, size * sqrt(3) ~/ 2, char);
   }
@@ -116,8 +116,8 @@ class IsoscelesTriangleCommand extends Command<String> {
 
   @override
   FutureOr<String>? run() {
-    final width = int.parse(argResults?['width'] ?? '50');
-    final height = int.parse(argResults?['height'] ?? '10');
+    final width = int.parse(argResults?['width'] as String? ?? '50');
+    final height = int.parse(argResults?['height'] as String? ?? '10');
     final char = (globalResults?['char'] as String?)?[0] ?? '#';
     return drawTriangle(width, height, char);
   }
